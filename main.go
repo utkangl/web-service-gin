6c480ecd236063d261dd5bbaa27b4303ae17ad73
utkan <utkngl@gmail.com>
2022-10-18 00:42:28 +0300

    get a spesific album by its id

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("127.0.0.1:8080")
@@ -32,7 +33,7 @@ var albums = []album{
 	{ID: "2", Title: "Sanirim Deliriyoruz", Artist: "Mertcan Kirci", Price: 69.99},
 }
 
-// the funciton responds with the list of albums as json file
+// the function responds with the list of albums as json file
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
@@ -44,17 +45,35 @@ func postAlbums(c *gin.Context) {
 	var newAlbum album
 
 	// if new object returns a error within BindJSON function, return nothing
-	if err:= c.BindJSON(&newAlbum); err != nil { 
-		return 
-	
-	// else if error doesn't occur, add the new album to albums slice of album struct
-	} else if err:= c.BindJSON(&newAlbum); err == nil { 
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+
+		// else if error doesn't occur, add the new album to albums slice of album struct
+	} else if err := c.BindJSON(&newAlbum); err == nil {
 
 		albums = append(albums, newAlbum)
-		
-		// IndentedJSON function takes an integer parameter(httpstatus as integer) 
+
+		// IndentedJSON function takes an integer parameter(httpstatus as integer)
 		// and an object, serializes the given object as JSON to convert to stream bytes and store it
-		c.IndentedJSON(http.StatusCreated, newAlbum) 
+		c.IndentedJSON(http.StatusCreated, newAlbum)
 	}
 
+}
+
+// the func to get a specific album by its id
+func getAlbumByID(c *gin.Context) {
+
+	// ??????
+	id := c.Param("id")
+
+	// if parameter id is equal to an album's id, return the album
+	for _, a := range albums {
+
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	// if requested album does not exist, display a message
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
